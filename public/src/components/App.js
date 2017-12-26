e78f5f24929b4d1cab779b4c593a958f4f03c700
junejie <junejieruzol1@gmail.com>
2017-12-26 13:07:49 +0800

    Update App.js

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 
 export default class App extends Component {
-	componentWillMount() {
+  componentWillMount() {
     this.props.loadUserFromToken();
   }
 
